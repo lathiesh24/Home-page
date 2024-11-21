28afc458554d3df037ce0caa231709322bcd70a8
lathiesh24 <lathumahi24@gmail.com>
2024-11-21 17:41:11 +0530

    section css added

--- components/sections/sections.js
+++ components/sections/sections.js
@@ -13,7 +13,16 @@ class Sections extends HTMLElement {
     this.render(otherSections);
   }
 
-  render(sections) {
+  async loadStyles() {
+  const response = await fetch("./components/sections/section.css");
+    if (!response.ok) {
+      console.error("Failed to load CSS file.");
+      return "";
+    }
+    return await response.text(); 
+  }
+
+  async render(sections) {
     const sectionsHTML = sections
       .map((section) => {
         return `
@@ -27,55 +36,9 @@ class Sections extends HTMLElement {
       })
       .join("");
 
+    const styles = await this.loadStyles(); 
     this.shadowRoot.innerHTML = `
-      <style>
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-        main {
-          display: flex;
-          flex-direction: column;
-          gap: 20px;
-          padding: 20px;
-          font-family: Arial, sans-serif;
-        }
-        .section {
-          border: 1px solid #ddd;
-          border-radius: 8px;
-          background: linear-gradient(135deg, #8e0e00, #d42589, #ff7e5f);          
-          padding: 16px;
-          text-align: center;
-        }
-        .section-image {
-          max-width: 100%;
-          border-radius: 8px;
-          margin-bottom: 12px;
-        }
-        .section-heading {
-          font-size: 1.25rem;
-          color: #ffffff;
-          margin-bottom: 8px;
-        }
-        .section-description {
-          font-size: 1rem;
-          color: #ffffff;
-          margin-bottom: 12px;
-        }
-        .cta-button {
-          display: inline-block;
-          padding: 8px 16px;
-          color: #d42589;
-          font-weight: bold;
-          border: 1;
-          background-color: #ffffff;
-          text-decoration: none;
-          border-radius: 4px;
-          font-size: 0.9rem;
-        }
-
-      </style>
+      <style>${styles}</style>
       <main>
         ${sectionsHTML}
       </main>
